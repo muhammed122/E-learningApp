41668577472354c5140a27893be819ecbb98e75b
ABDALLAH <abdalla167osman@gmail.com>
2021-02-19 16:32:15 +0200

    abdalla 7

--- app/src/main/java/com/example/e_learningapp/App.java
+++ app/src/main/java/com/example/e_learningapp/App.java
@@ -11,7 +11,8 @@ public class App extends Application {
 
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
         MySharedPrefrance.init(this);
     }
